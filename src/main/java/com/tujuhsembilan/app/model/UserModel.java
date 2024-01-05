ac8e403dbf6720b93bb10b833fd75130968e369c
madyardwn <madyardwn@gmail.com>
2024-01-05 10:44:38 +0700

    fix : (UserModel) change constraint

---
+++ src/main/java/com/tujuhsembilan/app/model/UserModel.java
@@ -0,0 +1,71 @@
+package com.tujuhsembilan.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class UserModel {
+  @Id
+  @Column(name = "user_id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID userId;
+
+  @Column(name = "role_id")
+  private UUID roleId;
+
+  @Column(name = "username", length = 50)
+  private String username;
+
+  @Column(name = "email", length = 100)
+  private String email;
+
+  @Column(name = "first_name", length = 255)
+  private String firstName;
+
+  @Column(name = "last_name", length = 255)
+  private String lastName;
+
+  @Column(name = "password", length = 255)
+  private String password;
+
+  @Column(name = "is_active")
+  private Boolean isActive;
+
+  @Column(name = "created_by")
+  @CreatedBy
+  private String createdBy;
+
+  @Column(name = "created_time")
+  @CreatedDate
+  private LocalDateTime createdTime;
+
+  @Column(name = "last_modified_by")
+  @LastModifiedBy
+  private String lastModifiedBy;
+
+  @Column(name = "last_modified_time")
+  @LastModifiedDate
+  private LocalDateTime lastModifiedTime;
+}
